7c6869c6aa34d8fed9cf2f73d7da3b4fadf7c653
Marcoant007 <marcoantnovo@gmail.com>
2026-02-08 10:58:33 -0300

    refactor: introduce component barrel file for centralized exports.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 // Layout Components
-export { Header } from './Layout/Header';
 export { Footer } from './Layout/Footer';
 
 // Brand Components
@@ -11,21 +10,3 @@ export { FiltersBar } from './Market/FiltersBar';
 
 // UI Components
 export * from './UI';
-
-// Feature Components (will be exported when they are refactored)
-// export { default as AccountSecurityView } from './AccountSecurityView';
-// export { default as ActivityView } from './ActivityView';
-// export { default as ApiDocsView } from './ApiDocsView';
-// export { default as BettingModal } from './BettingModal';
-// export { default as Chart } from './Chart';
-// export { default as DepositModal } from './DepositModal';
-// export { default as DocumentsView } from './DocumentsView';
-// export { default as LoginScreen } from './LoginScreen';
-// export { default as MarketTabs } from './MarketTabs';
-// export { default as PaymentGateway } from './PaymentGateway';
-// export { default as PaymentOptionsModal } from './PaymentOptionsModal';
-// export { default as PortfolioView } from './PortfolioView';
-// export { default as SettingsView } from './SettingsView';
-// export { default as SupportView } from './SupportView';
-// export { default as TradeSimulation } from './TradeSimulation';
-// export { default as TransfersView } from './TransfersView';
